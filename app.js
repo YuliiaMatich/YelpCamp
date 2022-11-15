4431511a05b5d13e6deb18469223c9b276d86ef2
YuliiaMatich <yuliia.matich@gmail.com>
2022-11-15 18:51:07 -0500

    add route create campground

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const Campground = require('./models/campground')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-  usenewUrlParser: true,
+  useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
 });
@@ -20,6 +20,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true}));
+
 
 app.get('/', (req, res) => {
   res.render('home')
@@ -30,11 +32,23 @@ app.get('/campgrounds', async (req, res) => {
   res.render('campgrounds/index', { campgrounds })
 })
 
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+  });
+
+app.post('/campgrounds', async(req, res) => {
+  const campground = new Campground (req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`)
+})
+
 app.get('/campgrounds/:id', async(req, res) => {
   const id = req.params.id;
   const campground = await Campground.findById(id);
   res.render('campgrounds/show', { campground });
 })
+
+
 
 app.listen(3000, () => {
   console.log("Serving on port 3000")
