34dae627fd2eb2604d61efa25931be02050d6541
YuliiaMatich <yuliia.matich@gmail.com>
2022-11-24 16:45:15 -0500

    validating and displaying reviews

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
-const { campgroundSchema} = require('./schemas.js');
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
@@ -40,9 +40,18 @@ const validateCampground = (req, res, next) => {
     throw new ExpressError(msg, 400)
   } else {
     next();
-  }
+  };
 }
 
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error){
+    const msg = error.details.map(el => el.message).join(',')
+    throw new ExpressError(msg, 400)
+  } else {
+    next();
+  }
+}
 
 
 app.get('/', (req, res) => {
@@ -67,7 +76,7 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
   const id = req.params.id;
-  const campground = await Campground.findById(id);
+  const campground = await Campground.findById(id).populate('reviews');
   res.render('campgrounds/show', { campground });
 }));
 
@@ -92,7 +101,7 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
 }));
 
 
-app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   const review = new Review(req.body.review);
   campground.reviews.push(review);
