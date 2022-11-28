0523f02b0d490f867ccce1b04749d4c70b99f868
YuliiaMatich <yuliia.matich@gmail.com>
2022-11-28 16:11:47 -0500

    set up flash messages

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const ExpressError = require('./utils/ExpressError');
@@ -15,7 +17,8 @@ const reviews = require('./routes/reviews');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useFindAndModify: false
 });
 
 const db = mongoose.connection;
@@ -32,6 +35,27 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+const sessionConfig = {
+  secret: 'thisschouldbeabettersecret!',
+  resave: false,
+  saveUnitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  }
+}
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error')
+  next();
+})
 
 
 app.use('/campgrounds', campgrounds);
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -6,9 +6,9 @@ const Campground = require('../models/campground');
 const { campgroundSchema, reviewSchema } = require('../schemas.js');
 
 const validateCampground = (req, res, next) => {
- 
+
   const { error } = campgroundSchema.validate(req.body);
-  if (error){
+  if (error) {
     const msg = error.details.map(el => el.message).join(',')
     throw new ExpressError(msg, 400)
   } else {
@@ -26,9 +26,9 @@ router.get('/new', (req, res) => {
 });
 
 router.post('/', validateCampground, catchAsync(async (req, res, next) => {
-  // if(!req.body.campground) throw new ExpressError('invalid Campground Data', 400)
-  const campground = new Campground (req.body.campground);
+  const campground = new Campground(req.body.campground);
   await campground.save();
+  req.flash('success', 'Successfully made a new campground');
   res.redirect(`/campgrounds/${campground._id}`)
 }));
 
