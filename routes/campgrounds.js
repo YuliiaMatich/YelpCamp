9efc8457398d92815ab6a3fa4a5f8d01553c1976
YuliiaMatich <yuliia.matich@gmail.com>
2022-12-06 11:13:28 -0500

    restructure campgrounds and users routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,25 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const Campground = require('../models/campground');
-const { reverse } = require('../seeds/cities');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
-const campgrounds = require ('../controllers/campgrounds');
+const campgrounds = require('../controllers/campgrounds');
 
-
-router.get('/', catchAsync(campgrounds.index));
+router.route('/')
+  .get(catchAsync(campgrounds.index))
+  .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+router.route('/:id')
+.get(catchAsync(campgrounds.showCampground))
+.put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+.delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
-router.get('/:id', catchAsync(campgrounds.showCampground));
+
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampground,  catchAsync(campgrounds.updateCampground));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
-
 
 module.exports = router;
