d51e9628446c68dd649d5fbf1b941235a90305a7
YuliiaMatich <yuliia.matich@gmail.com>
2022-12-07 18:07:07 -0500

    display images in a carousel

--- seeds/index.js
+++ seeds/index.js
@@ -26,9 +26,18 @@ const seedDB = async () => {
       author: '638e14381887bfb07e157320',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'https://source.unsplash.com/collection/483251',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In blanditiis autem ducimus voluptatibus adipisci vero necessitatibus! Reiciendis cupiditate sequi cumque a? Eos ut iure error, laudantium aliquam perferendis unde aliquid!',
-      price: price()
+      price: price(),
+      images: [
+      {
+        url: 'https://res.cloudinary.com/dotqe9fdf/image/upload/v1670453746/YelpCamp/orthdqatxzl5ldmo9jl4.jpg',
+        filename: 'YelpCamp/orthdqatxzl5ldmo9jl4'
+      },
+      {
+        url: 'https://res.cloudinary.com/dotqe9fdf/image/upload/v1670453747/YelpCamp/texmetaorp93yy61npip.jpg',
+        filename: 'YelpCamp/texmetaorp93yy61npip'
+      } 
+    ]
     })
     await camp.save();
   }
